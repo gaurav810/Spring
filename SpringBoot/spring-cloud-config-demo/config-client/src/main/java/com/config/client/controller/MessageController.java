8b84596baedd6bc773c83896e2e17072b54317ef
Gaurav Prajapati <gauravkumar.prajapati@neosofttech.com>
2020-01-20 17:31:21 +0530

    Added Spring cloud config project

---
+++ SpringBoot/spring-cloud-config-demo/config-client/src/main/java/com/config/client/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageController {
+
+	@Value("${message:Default Message}")
+	String message;
+	
+	@GetMapping("/")
+	public String testMessage() {
+		
+		return message;
+	}
+}

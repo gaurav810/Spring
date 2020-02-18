fe81f4275bf84a0c09378d94b018d5f9f51e0db7
gaurav810 <guruprajapati810@gmail.com>
2020-02-18 11:58:20 +0530

    spring boot + docker + maven + spotify

---
+++ SpringBoot/spring-boot-docker-maven/src/test/java/com/maven/docker/SpringBootDockerMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
